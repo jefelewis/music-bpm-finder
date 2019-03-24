b00898cd6b590ac260f7639a277bd0a8cc99bdf6
Jeff Lewis <jeffylew77@gmail.com>
2019-03-23 23:43:57 -0700

    [Dev] BPM + Beat decocding working

---
+++ musicBPMFinder.js
@@ -0,0 +1,46 @@
+// Imports: Dependencies
+const AudioContext = require("web-audio-api").AudioContext;
+const MusicTempo = require("music-tempo");
+const fs = require("fs");
+
+// Song
+let song = fs.readFileSync('./assets/TooBigForBiggie(Version 8).aif');
+
+// Find BPM
+const findBPM = async (buffer) => {
+  // Audio Data
+  let audioData = [];
+  // Take the average of the two channels
+  if (buffer.numberOfChannels == 2) {
+
+    // Channel Data
+    let channel1Data = await buffer.getChannelData(0);
+    let channel2Data = await buffer.getChannelData(1);
+
+    //
+    let length = channel1Data.length;
+    for (let i = 0; i < length; i++) {
+      audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+    }
+  }
+  
+  else {
+    audioData = await buffer.getChannelData(0);
+  }
+
+  // Song Data
+  let songData = await new MusicTempo(audioData);
+
+
+  // BPM (Tempo)
+  console.log(`BPM: ${Math.round(songData.tempo)}`);
+
+  // Beats
+  // console.log(`Beats: ${songData.beats}`);
+}
+
+// Context
+let context = new AudioContext();
+
+// Decode Audio: BPM (Tempo), Beats
+context.decodeAudioData(song, findBPM);
