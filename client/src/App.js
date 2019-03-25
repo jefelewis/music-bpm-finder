c0cc5ae5b338e385dcebbeed9d71a046c8c19ed5
Jeff Lewis <jeffylew77@gmail.com>
2019-03-25 01:07:56 -0700

    [Dev] Adding React frontend

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+// Imports: Dependencies
+import React, { Component } from 'react';
+
+// Imports: Components
+import BPM from './components/BPM';
+
+// Imports: CSS
+import './css/bpmFinder.css';
+
+// React Application
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <BPM />
+        </div>
+    );
+  }
+}
+
+// Exports
+export default App;
---
+++ client/src/components/BPM.js
@@ -0,0 +1,23 @@
+// Imports: Dependencies
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+// Component: BPM
+class BPM extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="title-container">
+          <h2 id="title-text">BPM Finder</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+// Exports
+export default BPM;
